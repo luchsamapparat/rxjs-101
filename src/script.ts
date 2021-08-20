99c40bf8e09153fd3071b1f2d453090665bcb714
Marvin Luchs <marvin.luchs@holisticon.de>
2021-08-20 23:31:17 +0200

    feat: 01 synchronous execution

---
+++ src/script.ts
@@ -0,0 +1,31 @@
+//#region Synchronous XHR Request
+
+function loadData() {
+    const request = new XMLHttpRequest();
+    request.open('GET', '/data.txt', false);
+    request.send(null);
+    return request.response;
+}
+
+const loadDataButton = document.querySelector<HTMLButtonElement>('#load-data-button')!;
+const dataContainer = document.querySelector<HTMLParagraphElement>('#data')!;
+
+loadDataButton.addEventListener('click', () => {
+    dataContainer.innerText = loadData()
+});
+
+//#endregion
+
+
+
+//#region Counter
+
+const counter = document.querySelector<HTMLParagraphElement>('#counter')!;
+
+let seconds = 1;
+setInterval(() => {
+    seconds += 1;
+    counter.innerText = seconds.toLocaleString();
+}, 1000)
+
+//#endregion
