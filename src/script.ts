984b806c0c408beed54a0fd2cabd869ef54a5eee
Marvin Luchs <marvin.luchs@holisticon.de>
2021-08-21 10:01:04 +0200

    refactor: text changes and renamings

--- src/script.ts
+++ src/script.ts
@@ -1,6 +1,33 @@
 //#region Asynchronous XHR Request
 
-function loadDataViaCallback(callback: (response: string) => void) {
+const loadDataCallbackButton = document.querySelector<HTMLButtonElement>('#load-data-callback-button')!;
+const loadDataPromiseButton = document.querySelector<HTMLButtonElement>('#load-data-promise-button')!;
+const loadDataAsyncButton = document.querySelector<HTMLButtonElement>('#load-data-async-button')!;
+const dataContainer = document.querySelector<HTMLParagraphElement>('#data')!;
+
+loadDataCallbackButton.addEventListener('click', () => {
+    dataContainer.innerText = '';
+    loadDataUsingXHR(
+        data => dataContainer.innerText = data
+    );
+});
+
+loadDataPromiseButton.addEventListener('click', () => {
+    dataContainer.innerText = '';
+    loadDataUsingFetch()
+        .then(
+            data => dataContainer.innerText = data
+        );
+});
+
+loadDataAsyncButton.addEventListener('click', async () => {
+    dataContainer.innerText = '';
+    const data = await loadDataUsingFetch();
+    dataContainer.innerText = data;
+});
+
+
+function loadDataUsingXHR(callback: (response: string) => void) {
     const request = new XMLHttpRequest();
     request.open('GET', '/data.txt', true);
     request.send(null);
@@ -11,35 +38,17 @@ function loadDataViaCallback(callback: (response: string) => void) {
     };
 }
 
-async function loadDataViaFetch() {
-    return (await window.fetch('/data.txt')).text();
+function loadDataUsingFetch() {
+    return window.fetch('/data.txt')
+        .then(response => response.text())
 }
 
-const loadDataCallbackButton = document.querySelector<HTMLButtonElement>('#load-data-callback-button')!;
-const loadDataPromiseButton = document.querySelector<HTMLButtonElement>('#load-data-promise-button')!;
-const loadDataAsyncButton = document.querySelector<HTMLButtonElement>('#load-data-async-button')!;
-const dataContainer = document.querySelector<HTMLParagraphElement>('#data')!;
-
-loadDataCallbackButton.addEventListener('click', () => {
-    dataContainer.innerText = '';
-    loadDataViaCallback(
-        data => dataContainer.innerText = data
-    );
-});
-
-loadDataPromiseButton.addEventListener('click', () => {
-    dataContainer.innerText = '';
-    loadDataViaFetch()
-        .then(
-            data => dataContainer.innerText = data
-        );
-});
-
-loadDataAsyncButton.addEventListener('click', async () => {
-    dataContainer.innerText = '';
-    const data = await loadDataViaFetch();
-    dataContainer.innerText = data;
-});
+// Promise-based implementation is interchangeable with async/await implementation:
+// 
+// async function loadDataUsingFetch() {
+//     const response = await window.fetch('/data.txt')
+//     return response.text();
+// }
 
 //#endregion
 
